c36e2955a67210024a5a6579d765681de54a57f5
upexcel <prateek@excellencetechnologies.in>
2019-05-21 18:59:29 +0530

    temp

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  apibase: 'https://api.taskular-dev.com/',
+  logoutRedirectUri: 'https://dashboard.taskular-dev.com/logout',
+  redirectUri: 'http://localhost:4200',
+  clientID: 'zbh5QTd6Q8dAi8ui09DOTbaGALWU6pzt',
+  domain: 'taskular-dev.au.auth0.com',
+  responseType: 'token id_token',
+  scope: 'openid profile name email user_metadata create:users',
+  metadateUri: 'https://taskular-dev:au:auth0:com/',
+};
